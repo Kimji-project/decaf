4991313eb719a227e1be07469081a9fa92175f24
Kimji <113660534+Kimji-project@users.noreply.github.com>
2022-10-14 16:42:58 +0900

    Delete script directory

--- script/script.js
+++
@@ -1,6 +0,0 @@
-$(".sub").hide();
-$(".gnb>ul>li")
-    .mouseenter(
-        function () { $('#header').css("background-color", "#123341") },
-        function () { $('.sub').stop().slideDown(300); })
-    .mouseleave(function () { $('.sub').stop().slideUp(300); });
